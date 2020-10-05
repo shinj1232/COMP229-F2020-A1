14da22807e07f744f50f52d665078d2eb8dde5dc
John Shin <john.shin53@gmail.com>
2020-10-05 17:12:41 -0400

    modified appjs2

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contact', contactRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
