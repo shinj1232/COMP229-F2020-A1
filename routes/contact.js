3b41a34209b29c27c2d3e58caa9d4b5c7c384e06
John Shin <john.shin53@gmail.com>
2020-10-05 17:46:52 -0400

    modified appjs4

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('PLACEHOLDER');
+    res.render('index', { contact: true });
 });
 
 module.exports = router;
